b0868cdeffb6fca3e332f576e6d6c65b25aba98d
Alex <a.malihin@it-justice.com>
2022-03-22 20:24:43 +0300

    complete currentcity

--- src/Pages/FullCardPage/FullCardPage.tsx
+++ src/Pages/FullCardPage/FullCardPage.tsx
@@ -9,8 +9,20 @@ export const FullCardPage = () => {
                 <h1 className={fullCardPageStyles.city_name}>Ростов-на-Дону</h1>
                 <div className={fullCardPageStyles.weather_days}>
                     <h2 className={fullCardPageStyles.days_headers}>Сегодня</h2>
+                    <div className={fullCardPageStyles.cards}>
+                        <div className={fullCardPageStyles.card_block}>d</div>
+                        <div className={fullCardPageStyles.card_block}>d</div>
+                        <div className={fullCardPageStyles.card_block}>d</div>
+                        <div className={fullCardPageStyles.card_block}>d</div>
+                    </div>
                     <h2 className={fullCardPageStyles.days_headers}>Завтра</h2>
+                    <div>
+
+                    </div>
                     <h2 className={fullCardPageStyles.days_headers}>23 Марта</h2>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         </div>);
