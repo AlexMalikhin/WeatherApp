a0d6ad9dc4bd95325843196f46c3cda9e604aec0
Alex <a.malihin@it-justice.com>
2022-03-30 20:53:12 +0300

    refactoring...

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,16 +3,20 @@ import {FullCardPage} from "../FullCardPage/FullCardPage";
 import {CityCard} from "../../components/CityCard/CityCard";
 import {RootState} from "../../store";
 import mainPageStyles from './MainPage.module.scss';
+import {ParallaxBackground} from "../../components/ParallaxBackground/ParallaxBackground";
+import {useEffect, useState} from "react";
 
-export const MainPage = () =>{
-    const {allCities} = useSelector((state:RootState)=> state.citiesReducer)
-    const {isOpenFullPage} = useSelector((state:RootState)=>state.modalFullForecastReducer)
+export const MainPage = () => {
+    const {allCities} = useSelector((state: RootState) => state.citiesReducer)
+    const {isOpenFullPage} = useSelector((state: RootState) => state.modalFullForecastReducer)
 
-    return(
+
+    return (
         <div className={mainPageStyles.wrapper}>
-            {isOpenFullPage && <FullCardPage />}
+            {isOpenFullPage && <FullCardPage/>}
+            <ParallaxBackground />
             <div className={isOpenFullPage ? mainPageStyles.container_hidden : mainPageStyles.container}>
-                {allCities?.map((currentCity:any)=>(
+                {allCities?.map((currentCity: any) => (
                     <CityCard
                         key={currentCity.id}
                         cityData={currentCity}
